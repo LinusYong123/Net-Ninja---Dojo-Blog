9a3dae2a4345ae8fe64b060e113a9e7643f9c57c
Linus Yong <86176685+LinusYong123@users.noreply.github.com>
2021-09-17 01:15:06 +0000

    ep.21 created loading

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,16 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     fetch('http://localhost:8000/blogs')
-  //     .then(res => {
-  //       return res.json();
-  //     })
-  //     .then(data => {
-  //       setIsPending(false);
-  //       setBlogs(data);
-  //     })
-  //   }, 1000);
-  // }, [])
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('https://github.com/LinusYong123/demo')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-      })
-    }, 1000);
-  }, [])
-
+  const { data, isPending, error } = useFetch('https://my-json-server.typicode.com/LinusYong123/demo/blogs');
 
   return (
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      {blogs && <BlogList blogs={blogs} />}
+      { data && <BlogList blogs={data} /> }
     </div>
   );
 }
